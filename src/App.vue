29d04b23e4d118c93194b83e528d8cbc6b0319ab
Igor Volchenok <checklogon@gmail.com>
2022-01-07 15:32:12 +0700

    chore: add Axios

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
   <el-form>
     <el-form-item>
-      <el-input v-model="url" placeholder="enter the link to your website">
+      <el-input v-model="pageUrl" placeholder="enter the link to your website">
       </el-input>
     </el-form-item>
     <el-button type="primary" @click="sendUrl">Submit</el-button>
   </el-form>
+  <h2>Heatmap</h2>
+  <div
+    class="heatmap__container"
+    :style="{ backgroundImage: 'url(' + mediaLink + ')' }"
+  ></div>
 </template>
 
 <script>
 /* eslint-disable */
+import axios from "axios";
 export default {
   name: "App",
   data() {
     return {
-      url: ''
-    }
+      pageUrl: "",
+      mediaLink:""
+    };
   },
   methods: {
-    sendUrl() {
-      console.log('url:', this.url)
+    async sendUrl() {
+      console.log("pageUrl:", this.pageUrl);
+      const response = await axios.post(
+        "https://us-central1-heatmapp-get.cloudfunctions.net/screenshotProcessing",
+        // "https://us-central1-heatmap-e4549.cloudfunctions.net/screenshotProcessing",
+        // "http://localhost:5001/heatmap-e4549/us-central1/screenshotProcessing",
+        { screenshotUrl: this.pageUrl }
+      );
+      console.log('response.data', response.data)
+      this.mediaLink = response.data.mediaLink
     }
-  }
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -33,5 +48,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.heatmap {
+&__container {
+  background-color: grey;
+  height: 800px;
+  width: 600px;
+}
 }
 </style>
